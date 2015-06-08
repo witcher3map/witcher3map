f7c94f4ae06cea2b42c16ae1bbdfb84528297e38
untamed0 <untamed0@googlemail.com>
2015-06-08 01:52:58 +0100

    switch to relative urls, fixes #8

--- files/js/shared.js
+++ files/js/shared.js
@@ -71,132 +71,132 @@
 	};
 
 	icons.abandoned = L.icon({
-		iconUrl  : '/files/img/icons/abandoned.png',
+		iconUrl  : '../files/img/icons/abandoned.png',
 		iconSize : [30, 30]
 	});
 
 	icons.alchemy = L.icon({
-		iconUrl  : '/files/img/icons/alchemy.png',
+		iconUrl  : '../files/img/icons/alchemy.png',
 		iconSize : [20, 28]
 	});
 
 	icons.armourer = L.icon({
-		iconUrl  : '/files/img/icons/armourer.png',
+		iconUrl  : '../files/img/icons/armourer.png',
 		iconSize : [24, 34]
 	});
 
 	icons.armourerstable = L.icon({
-		iconUrl  : '/files/img/icons/armourerstable.png',
+		iconUrl  : '../files/img/icons/armourerstable.png',
 		iconSize : [30, 27]
 	});
 
 	icons.banditcamp = L.icon({
-		iconUrl  : '/files/img/icons/banditcamp.png',
+		iconUrl  : '../files/img/icons/banditcamp.png',
 		iconSize : [29, 30]
 	});
 
 	icons.barber = L.icon({
-		iconUrl  : '/files/img/icons/barber.png',
+		iconUrl  : '../files/img/icons/barber.png',
 		iconSize : [30, 30]
 	});
 
 	icons.blacksmith = L.icon({
-		iconUrl  : '/files/img/icons/blacksmith.png',
+		iconUrl  : '../files/img/icons/blacksmith.png',
 		iconSize : [27, 30]
 	});
 
 	icons.brothel = L.icon({
-		iconUrl  : '/files/img/icons/brothel.png',
+		iconUrl  : '../files/img/icons/brothel.png',
 		iconSize : [28, 26]
 	});
 
 	icons.entrance = L.icon({
-		iconUrl  : '/files/img/icons/entrance.png',
+		iconUrl  : '../files/img/icons/entrance.png',
 		iconSize : [28, 27]
 	});
 
 	icons.grindstone = L.icon({
-		iconUrl  : '/files/img/icons/grindstone.png',
+		iconUrl  : '../files/img/icons/grindstone.png',
 		iconSize : [30, 26]
 	});
 
 	icons.guarded = L.icon({
-		iconUrl  : '/files/img/icons/guarded.png',
+		iconUrl  : '../files/img/icons/guarded.png',
 		iconSize : [23, 34]
 	});
 
 	icons.gwent = L.icon({
-		iconUrl  : '/files/img/icons/gwent.png',
+		iconUrl  : '../files/img/icons/gwent.png',
 		iconSize : [24, 30]
 	});
 
 	icons.harbor = L.icon({
-		iconUrl  : '/files/img/icons/harbor.png',
+		iconUrl  : '../files/img/icons/harbor.png',
 		iconSize : [27, 30]
 	});
 
 	icons.herbalist = L.icon({
-		iconUrl  : '/files/img/icons/herbalist.png',
+		iconUrl  : '../files/img/icons/herbalist.png',
 		iconSize : [25, 28]
 	});
 
 	icons.hidden = L.icon({
-		iconUrl  : '/files/img/icons/hidden.png',
+		iconUrl  : '../files/img/icons/hidden.png',
 		iconSize : [23, 34]
 	});
 
 	icons.innkeep = L.icon({
-		iconUrl  : '/files/img/icons/tavern.png',
+		iconUrl  : '../files/img/icons/tavern.png',
 		iconSize : [26, 30]
 	});
 
 	icons.monsterden = L.icon({
-		iconUrl  : '/files/img/icons/monsterden.png',
+		iconUrl  : '../files/img/icons/monsterden.png',
 		iconSize : [30, 27]
 	});
 
 	icons.monsternest = L.icon({
-		iconUrl  : '/files/img/icons/monsternest.png',
+		iconUrl  : '../files/img/icons/monsternest.png',
 		iconSize : [23, 30]
 	});
 
 	icons.notice = L.icon({
-		iconUrl  : '/files/img/icons/notice.png',
+		iconUrl  : '../files/img/icons/notice.png',
 		iconSize : [23, 28]
 	});
 
 	icons.pid = L.icon({
-		iconUrl  : '/files/img/icons/pid.png',
+		iconUrl  : '../files/img/icons/pid.png',
 		iconSize : [24, 34]
 	});
 
 	icons.pop = L.icon({
-		iconUrl  : '/files/img/icons/pop.png',
+		iconUrl  : '../files/img/icons/pop.png',
 		iconSize : [27, 30]
 	});
 
 	icons.poi = L.icon({
-		iconUrl  : '/files/img/icons/poi.png',
+		iconUrl  : '../files/img/icons/poi.png',
 		iconSize : [28, 28]
 	});
 
 	icons.shopkeeper = L.icon({
-		iconUrl  : '/files/img/icons/merchant.png',
+		iconUrl  : '../files/img/icons/merchant.png',
 		iconSize : [21, 30]
 	});
 
 	icons.signpost = L.icon({
-		iconUrl  : '/files/img/icons/fasttravel.png',
+		iconUrl  : '../files/img/icons/fasttravel.png',
 		iconSize : [27, 34]
 	});
 
 	icons.smugglers = L.icon({
-		iconUrl  : '/files/img/icons/smugglers.png',
+		iconUrl  : '../files/img/icons/smugglers.png',
 		iconSize : [28, 30]
 	});
 
 	icons.spoils = L.icon({
-		iconUrl  : '/files/img/icons/spoils.png',
+		iconUrl  : '../files/img/icons/spoils.png',
 		iconSize : [25, 28]
 	});
 }());
