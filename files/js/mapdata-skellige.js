12714754cab1c8410d433475560d09fd23583cb7
mcarver <mcarver@gmail.com>
2015-06-24 17:42:50 -0400

    finish skellige i18n reformatting, ahh.

--- files/js/mapdata-skellige.js
+++ files/js/mapdata-skellige.js
@@ -963,228 +963,228 @@ $(document).on("loadMapdata", function () {
 	// Sign Post
 		signpost: [{ // Faroe
 			coords: [[-77.490, 69.829]],
-			label: 'Trottheim',
-			popup: 'This village was once famed across all of Skellige for the fact that it was inhabited exclusively by women, all their men having died during one raid or another'
+			label: $.t("s:signpost.label.trottheim"),
+			popup: $.t("s:signpost.desc.trottheim")
 		}, {
 			coords: [[-77.206, 49.526]],
-			label: 'Harviken',
-			popup: 'Home seat to Clan Dimun, and the location where Holger Blackhand divvies out the loot after every successful raid'
+			label: $.t("s:signpost.label.harviken"),
+			popup: $.t("s:signpost.desc.harviken")
 		}, { // Hindarsfjall
 			coords: [[-30.031, 99.272]],
-			label: 'Larvik',
-			popup: 'The largest village on Hindarsfjall and home seat to Donar, head of Clan an Hindar. Its inhabitants are just and god-fearing folk, traditionalists strident in their devotion to Freya'
+			label: $.t("s:signpost.label.larvik"),
+			popup: $.t("s:signpost.desc.larvik")
 		}, {
 			coords: [[-20.838, 86.177]],
-			label: 'Freya\'s Garden',
-			popup: 'Garden dedicated to the goddess Freya. Once beautiful and filled with thousands of fragrant blooms and herbs, today it lies abandoned and untended'
+			label: $.t("s:signpost.label.freyasGarden"),
+			popup: $.t("s:signpost.desc.freyasGarden")
 		}, {
 			coords: [[-25.463, 81.563]],
-			label: 'Lofoten',
-			popup: 'Once a rich and vibrant village, today Lofoten is a ravaged and crumbling ruin'
+			label: $.t("s:signpost.label.lofoten"),
+			popup: $.t("s:signpost.desc.lofoten")
 		}, {
 			coords: [[-29.764, 82.375]],
-			label: 'Lofoten Cemetery',
-			popup: 'The inhabitants of Lofoten often visit this small cemetery to care for the graves of their loved ones and ask Freya for blessings in the afterlife'
+			label: $.t("s:signpost.label.lofotenCemetery"),
+			popup: $.t("s:signpost.desc.lofotenCemetery")
 		}, {
 			coords: [[-33.505, 85.144]],
-			label: 'Isolated Hut',
-			popup: 'They say this was once home to a herbalist who came to the isles from the continent. Unable to find a place in any of the nearby villages, she settled in this seaside hut, where she recieved the occasional visitor in need of magic creams of bandages'
+			label: $.t("s:signpost.label.isolatedHut"),
+			popup: $.t("s:signpost.desc.isolatedHut")
 		}, {
 			coords: [[-36.668, 91.604]],
-			label: 'Lurthen',
-			popup: 'Lurthen\'s most famous inhabitant was a certain Peter Pijus, known for the fact that he was able to drink an entire barrel of mead in one go and remain standing'
+			label: $.t("s:signpost.label.lurthen"),
+			popup: $.t("s:signpost.desc.lurthen")
 		}, { // An Skellig
 			coords: [[52.882, 46.230]],
-			label: 'Trail to Yngvar\'s Fang',
-			popup: 'This is the start of the mountain trail leading to Yngvar\'s Fang, the pride and glory of An Skellig'
+			label: $.t("s:signpost.label.trailToYngvarsFang"),
+			popup: $.t("s:signpost.desc.trailToYngvarsFang")
 		}, {
 			coords: [[54.623, 35.376]],
-			label: 'Yngvar\'s Fang',
-			popup: 'This mountain peak was named after the mythical bear which, according to legend, was defeated by Tyr, the heroic founder of Clan Tuirsearch'
+			label: $.t("s:signpost.label.yngvarsFang"),
+			popup: $.t("s:signpost.desc.yngvarsFang")
 		}, {
 			coords: [[50.092, 38.364]],
-			label: 'Urialla Harbor',
-			popup: 'On account of their mastery of their craft and painstaking attention to detail, the shipwrights working in this harbor are considered the best in the isles'
+			label: $.t("s:signpost.label.uriallaHarbor"),
+			popup: $.t("s:signpost.desc.uriallaHarbor")
 		}, {
 			coords: [[48.444, 27.510]],
-			label: 'Bay Of Winds',
-			popup: 'Three generations ago this bay was a popular meeting spot for the local youth. Then one night a terrible storm broke out and the sea pounded into the beach, swallowing up several merry-makers and dragging them out to a watery grave'
+			label: $.t("s:signpost.label.bayOfWinds"),
+			popup: $.t("s:signpost.desc.bayOfWinds")
 		}, { // Spikeroog
 			coords: [[33.229, -99.470]],
-			label: 'Hov',
-			popup: 'Village known throughout all of Skellige for the infamous arena which once hosted fierce fights between the mightiest warriors in the isles'
+			label: $.t("s:signpost.label.hov"),
+			popup: $.t("s:signpost.desc.hov")
 		}, {
 			coords: [[32.380, -113.005]],
-			label: 'Svorlag',
-			popup: 'The village was founded by the mythical Sove, who killed a terrifying and bloodthirsty chimera on this spot'
+			label: $.t("s:signpost.label.svorlag"),
+			popup: $.t("s:signpost.desc.svorlag")
 		}, {
 			coords: [[22.289, -121.509]],
-			label: 'Old Watchtower',
-			popup: 'The ruins of an old watchtower in which, according to legend, a crazed Koviri princess once locked herself up, convinced only a man able to free her from this tower would be fit to be her husband. No one even tried, however, the princess died of old age and the tower fell into disrepair with the passage of time'
+			label: $.t("s:signpost.label.oldWatchtower"),
+			popup: $.t("s:signpost.desc.oldWatchtower")
 		}, { // Eastern Islands
 			coords: [[-16.046, -139.482]],
-			label: 'The Pali Gap Coast',
-			popup: 'Elders say an isolated cave on this coast was once used as a retreat by the world\'s most famous bard (before the rise of Dandelion, that is): the great Xirdneh of Zanguebar, renowned from Nazair to the Dragon Mountains for his ferocious lute-strumming'
+			label: $.t("s:signpost.label.thePaliGapCoast"),
+			popup: $.t("s:signpost.desc.thePaliGapCoast")
 		}, {
 			coords: [[-8.538, -94.922]],
-			label: 'Kaer Almhult',
-			popup: 'Built centuries ago to serve as the home keep for the kings of Skellige. In practice, however, each ruler preferred to keep to his clan\'s seat, and Kaer Almhult was left unused. Eventually the decision was made to turn it into a prison, and today the fortress is a crumbling ruin'
+			label: $.t("s:signpost.label.kaerAlmhult"),
+			popup: $.t("s:signpost.desc.kaerAlmhult")
 		}, { // Undvik
 			coords: [[-52.456, -110.391]],
-			label: 'Marlin Coast',
-			popup: 'Until quite recently this beach was frequented by fishermen come to fish marlins out of the nearby waters'
+			label: $.t("s:signpost.label.marlinCoast"),
+			popup: $.t("s:signpost.desc.marlinCoast")
 		}, {
 			coords: [[-58.984, -98.899]],
-			label: 'Gull Point',
-			popup: 'Sea fowl come from miles around to congregate on this scrap of barren land, mate and lay eggs'
+			label: $.t("s:signpost.label.gullPoint"),
+			popup: $.t("s:signpost.desc.gullPoint")
 		}, {
 			coords: [[-56.377, -113.533]],
-			label: 'Dorve Ruins',
-			popup: 'Ruins of a village destroyed by the Ice Giant'
+			label: $.t("s:signpost.label.dorveRuins"),
+			popup: $.t("s:signpost.desc.dorveRuins")
 		}, {
 			coords: [[-61.365, -121.553]],
-			label: 'Clan Tordarroch Forge',
-			popup: 'A famous forge where the best tools and weapons in all of Skellige were once made. When the Ice Giant took over the isle, he turned it into his larder'
+			label: $.t("s:signpost.label.clanTordarrochForge"),
+			popup: $.t("s:signpost.desc.clanTordarrochForge")
 		}, {
 			coords: [[-58.367, -127.529]],
-			label: 'Urskar',
-			popup: 'The villagers here were famous for their love of pickled herring. They ate it for breakfast, dinner, and supper, and even made jams and compotes out of it'
+			label: $.t("s:signpost.label.urskar"),
+			popup: $.t("s:signpost.desc.urskar")
 		}, {
 			coords: [[-54.801, -135.176]],
-			label: 'Abandoned Village',
-			popup: 'This village\'s residents were forced to abandon it in a hurry when the Ice Giant unexpectedly awoke and decided to make known his wrath'
+			label: $.t("s:signpost.label.abandonedVillage"),
+			popup: $.t("s:signpost.desc.abandonedVillage")
 		}, {
 			coords: [[-43.133, -139.219]],
-			label: 'Tor Gvalch\'ca',
-			popup: 'An ancient tower which was erected in the days when elves were the unchallenged masters of these lands'
+			label: $.t("s:signpost.label.torGvalchca"),
+			popup: $.t("s:signpost.desc.torGvalchca")
 		}, { // Ard Skellig
 			coords: [[-70.707, -6.064]],
-			label: 'Elverum Lighthouse',
-			popup: 'The former keeper of this lighthouse was a confirmed eccentric. In addition to caring for the lighthouse, he also wrote poetry and wove carpets, was known to strip naked and run laps around the lighthouse at noon while shouting "sound mind in a sound body" and for breakfast would eat nothing but fish tails'
+			label: $.t("s:signpost.label.elverumLighthouse"),
+			popup: $.t("s:signpost.desc.elverumLighthouse")
 		}, {
 			coords: [[-58.939, -3.252]],
-			label: 'Ruined Inn',
-			popup: 'Ruined and burned-down tavern which once treated locals and travelers to the best roast lamb around'
+			label: $.t("s:signpost.label.ruinedInn"),
+			popup: $.t("s:signpost.desc.ruinedInn")
 		}, {
 			coords: [[-55.004, -15.029]],
-			label: 'Fyresdal',
-			popup: 'Those living here are tough on the outside, but soft and tender within'
+			label: $.t("s:signpost.label.fyresdal"),
+			popup: $.t("s:signpost.desc.fyresdal")
 		}, {
 			coords: [[-63.085, -38.496]],
-			label: 'Kaer Muire',
-			popup: 'This fortress has served as Clan Drummond\'s home base for centuries. the days when it was new and in full repair are a distant memory now'
+			label: $.t("s:signpost.label.kaerMuire"),
+			popup: $.t("s:signpost.desc.kaerMuire")
 		}, {
 			coords: [[-64.539, -47.329]],
-			label: 'Holmstein\'s Port',
-			popup: 'Piers and docks for the village of Holmstein - Clan Drummond\'s chief port'
+			label: $.t("s:signpost.label.holmsteinsPort"),
+			popup: $.t("s:signpost.desc.holmsteinsPort")
 		}, {
 			coords: [[-54.098, -60.754]],
-			label: 'Wild Shore',
-			popup: 'A wild and untamed&trade; part of the isle\'s coastline. A favourite spot for bandits and lovers'
+			label: $.t("s:signpost.label.wildShore"),
+			popup: $.t("s:signpost.desc.wildShore")
 		}, {
 			coords: [[-50.958, -42.935]],
-			label: 'Fornhala',
-			popup: 'Mysterious abandoned village'
+			label: $.t("s:signpost.label.fornhala"),
+			popup: $.t("s:signpost.desc.fornhala")
 		}, {
 			coords: [[-54.763, 12.964]],
-			label: 'Distillery',
-			popup: 'A shroud of secrecy envelops this place. All that is known is that it produces the finest hooch in all of skellige'
+			label: $.t("s:signpost.label.distillery"),
+			popup: $.t("s:signpost.desc.distillery")
 		}, {
 			coords: [[-56.837, 23.071]],
-			label: 'Grotto',
-			popup: 'An isolated-off locale that can only be reached by boat'
+			label: $.t("s:signpost.label.grotto"),
+			popup: $.t("s:signpost.desc.grotto")
 		}, {
 			coords: [[-47.145, -6.812]],
-			label: 'Palisade',
-			popup: 'The remnants of the palisade that once marked the border between the territories of Clan Drummond and Clan an Craite'
+			label: $.t("s:signpost.label.palisade"),
+			popup: $.t("s:signpost.desc.palisade")
 		}, {
 			coords: [[-43.165, -63.677]],
-			label: 'Arinbjorn',
-			popup: 'A village whose calm is only occasionally disturbed by someone slapping another senseless or one comrade breaking a bottle of mead over his mate\'s head'
+			label: $.t("s:signpost.label.arinbjorn"),
+			popup: $.t("s:signpost.desc.arinbjorn")
 		}, {
 			coords: [[-40.212, -47.900]],
-			label: 'Sund',
-			popup: 'Small village whose inhabitant are for the most part shepherds'
+			label: $.t("s:signpost.label.sund"),
+			popup: $.t("s:signpost.desc.sund")
 		}, {
 			coords: [[-36.351, -31.311]],
-			label: 'Fayrlund',
-			popup: 'This small village has gained great renown as home to the best hunters in Skellige'
+			label: $.t("s:signpost.label.fayrlund"),
+			popup: $.t("s:signpost.desc.fayrlund")
 		}, {
 			coords: [[-41.311, -17.886]],
-			label: 'Boxholm',
-			popup: 'Boxholm was once a thriving village serving the nearby fortress, Kaer Nyssen. Today nothing remains of this past glory but a pile of stones, some debris, and fading memories'
+			label: $.t("s:signpost.label.boxholm"),
+			popup: $.t("s:signpost.desc.boxholm")
 		}, {
 			coords: [[-29.306, -25.928]],
-			label: 'Rannvaig',
-			popup: 'Fifteen years ago, one of the fishermen of Rannvaig bagged an enormous halibut, and from that moment on all the other villagers have devoted their lives to beating his record'
+			label: $.t("s:signpost.label.rannvaig"),
+			popup: $.t("s:signpost.desc.rannvaig")
 		}, {
 			coords: [[-30.827, -4.219]],
-			label: 'Blandare',
-			popup: 'Unusually for a Skellige village, Blandare is located inland, far from any shore. Its inhabitants scratch out a living through mining and shepherding'
+			label: $.t("s:signpost.label.blandare"),
+			popup: $.t("s:signpost.desc.blandare")
 		}, {
 			coords: [[-32.064, 14.458]],
-			label: 'Druids\' Camp',
-			popup: 'Camp pitched by druids investigating the magic cataclysm which devastated the nearby woods'
+			label: $.t("s:signpost.label.druidsCamp"),
+			popup: $.t("s:signpost.desc.druidsCamp")
 		}, {
 			coords: [[-30.940, 25.356]],
-			label: 'Redgill',
-			popup: 'This village\'s inhabitants fled in a panic when a mysterious magic cataclysm struck the surrounding area'
+			label: $.t("s:signpost.label.redgill"),
+			popup: $.t("s:signpost.desc.redgill")
 		}, {
 			coords: [[-25.642, 7.031]],
-			label: 'Abandoned Sawmill',
-			popup: 'Though located depp in the forest, an ideal place for lumber harvesting, the sawmill now lies abandoned and unused'
+			label: $.t("s:signpost.label.abandonedSawmill"),
+			popup: $.t("s:signpost.desc.abandonedSawmill")
 		}, {
 			coords: [[-22.614, 12.986]],
-			label: 'Gedyneith',
-			popup: 'An oak which is sacred to all Skelligers, as it is worshiped both by druids and Freya\'s disciples. The Isle\'s most important ceremonies are held here, including weddings and royal coronations'
+			label: $.t("s:signpost.label.gedyneith"),
+			popup: $.t("s:signpost.desc.gedyneith")
 		}, {
 			coords: [[-22.533, 17.996]],
-			label: 'Gedyneith',
-			popup: 'An oak which is sacred to all Skelligers, as it is worshiped both by druids and Freya\'s disciples. The Isle\'s most important ceremonies are held here, including weddings and royal coronations'
+			label: $.t("s:signpost.label.gedyneith2"),
+			popup: $.t("s:signpost.desc.gedyneith2")
 		}, {
 			coords: [[-13.475, 24.390]],
-			label: 'Whale Graveyard',
-			popup: 'The isle\'s inhabitants come here and gather bones which they use to build their huts'
+			label: $.t("s:signpost.label.whaleGraveyard"),
+			popup: $.t("s:signpost.desc.whaleGraveyard")
 		}, {
 			coords: [[-21.002, -30.059]],
-			label: 'Crossroads',
-			popup: 'Fork in the road leading to Kaer Trolde'
+			label: $.t("s:signpost.label.crossroads"),
+			popup: $.t("s:signpost.desc.crossroads")
 		}, {
 			coords: [[-16.341, -9.404]],
-			label: 'Miners\' Camp',
-			popup: 'Small camp which local miners use as a base during their gold prospecting expeditions into the mountains to the north'
+			label: $.t("s:signpost.label.minersCamp"),
+			popup: $.t("s:signpost.desc.minersCamp")
 		}, {
 			coords: [[-14.520, -70.928]],
-			label: 'Eldberg Lighthouse',
-			popup: 'Lighthouse build on orders of Jarl Skjordal in order to light the sea and route to Arinbjorn'
+			label: $.t("s:signpost.label.eldbergLighthouse"),
+			popup: $.t("s:signpost.desc.eldbergLighthouse")
 		}, {
 			coords: [[-12.512, 1.626]],
-			label: 'Kaer Gelen',
-			popup: 'This old fort\'s ruins attract the sort whom one would not want to run into in a dark alley'
+			label: $.t("s:signpost.label.kaerGelen"),
+			popup: $.t("s:signpost.desc.kaerGelen")
 		}, {
 			coords: [[-7.067, -37.617]],
-			label: 'Kaer Trolde Harbor',
-			popup: 'One of the busiest ports in Skellige. Goods from every corner of the world are brought here to be sold and traded'
+			label: $.t("s:signpost.label.kaerTroldeHarbor"),
+			popup: $.t("s:signpost.desc.kaerTroldeHarbor")
 		}, {
 			coords: [[2.636, -38.650]],
-			label: 'Bridge to Kaer Trolde',
-			popup: 'This stone bridge was, according to legend, carved single-handedly by Grymmdjarr, the heroic founder of Clan an Craite'
+			label: $.t("s:signpost.label.bridgeToKaerTrolde"),
+			popup: $.t("s:signpost.desc.bridgeToKaerTrolde")
 		}, {
 			coords: [[-8.494, -18.171]],
-			label: 'Rogne',
-			popup: 'This mountain settlement is home to tough folk of indomitable spirit'
+			label: $.t("s:signpost.label.rogne"),
+			popup: $.t("s:signpost.desc.rogne")
 		}, {
 			coords: [[-1.384, -1.956]],
-			label: 'Yustianna\'s Grotto',
-			popup: 'Yustianna was a pirate born of a Skelliger from Clan an Craite and a captive woman taken during a raid. In her time she was feared on the Continent from Ofir to Zanguebar. Known for her skill as a navigator and unmatched master of various weapons, she quickly became the terror of the Great Sea, and when she returned to Skellige, they say this cave is where she hid her loot'
+			label: $.t("s:signpost.label.yustiannasGrotto"),
+			popup: $.t("s:signpost.desc.yustiannasGrotto")
 		}, {
 			coords: [[2.724, 15.029]],
-			label: 'Giants\' Toes',
-			popup: 'In truth simply a normal rock formation shaped by centuries of wind and water, islanders believe Uroboros punished giants who opposed his will by turning them into these stones'
+			label: $.t("s:signpost.label.giantsToes"),
+			popup: $.t("s:signpost.desc.giantsToes")
 		}, {
 			coords: [[9.947, -22.039]],
-			label: 'Ancient Crypt',
-			popup: 'Though Skelligers are famed for bravery bordering on madness, there are certain places which even they keep their distance from. This is one of them'
+			label: $.t("s:signpost.label.ancientCrypt"),
+			popup: $.t("s:signpost.desc.ancientCrypt")
 		}],
 
 	// Smugglers' Cache
