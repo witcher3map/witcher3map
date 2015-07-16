788aec6b9d6ef6370fa153b2b4055ba4bc60d414
mcarver <mcarver@gmail.com>
2015-07-16 08:58:12 -0400

    final commit of note functionality

--- files/js/custom.js
+++ files/js/custom.js
@@ -150,14 +150,9 @@ $(document).on("loadCustom", function() {
 
 	$('.leaflet-marker-icon').on('contextmenu',function(e){ return false; });
 
-	map.on('click', function(e) {
-		console.log('Clicked at:');
-		console.log('[' + e.latlng.lat.toFixed(3) + ', ' + e.latlng.lng.toFixed(3) + ']');
-	});
-
 	map.on('popupopen', function(e) {
 		deleteCircle();
-		createCircle(e.popup._latlng.lat.toFixed(3),e.popup._latlng.lng.toFixed(3));
+		createCircle(e.popup._latlng.lat, e.popup._latlng.lng);
 		$('#info-wrap').stop();
 		if (localStorage['sfw'] && e.popup._source._popup._content.match(/prostitute/i)) {
 			$('#info').html('<h1>' + $.t('sidebar.loveInterest') + '</h1>' + $.t('misc.loveInterestDesc'));
@@ -166,19 +161,24 @@ $(document).on("loadCustom", function() {
 		}
 		$('#info').getNiceScroll(0).doScrollTop(0,0);
 		$('#info-wrap').fadeIn('fast');
+		$('#info').i18n();
 		console.log('Popup at:');
 		console.log('[' + e.popup._latlng.lat.toFixed(3) + ', ' + e.popup._latlng.lng.toFixed(3) + ']');
 	});
 
-	var createCircle = function(lat, long) {
-		hash.addParam('m', lat + ',' + long);
-		circle = L.circleMarker(L.latLng(lat,long), {
+	var createCircle = function(lat, lng) {
+		var noteKey = getNoteKey(lat, lng);
+		//only add param and show center button if not a note
+		if(!notes[map_path][getNoteIndex(noteKey)]) {
+			hash.addParam('m', lat + ',' + lng);
+		 	$('#centerButton').show();
+		}
+		circle = L.circleMarker(L.latLng(lat, lng), {
 			color: 'red',
 			fillColor: '#f03',
 			fillOpacity: 0.5,
 			radius: 20
 		}).addTo(map);
-		$('#centerButton').show();
 	};
 
 	var deleteCircle = function() {
@@ -189,12 +189,16 @@ $(document).on("loadCustom", function() {
 		}
 	};
 
-	map.on('popupclose', function(e) {
+	var popupClose = function() {
 		$('#info-wrap').fadeOut('fast', function() {
 			$('#info').html('');
 			deleteCircle();
 			map.closePopup();
 		});
+	};
+
+	map.on('popupclose', function(e) {
+		popupClose();
 	});
 
 	if (localStorage['markers-' + window.map_path]) {
@@ -206,7 +210,7 @@ $(document).on("loadCustom", function() {
 		});
 	}
 
-  $('ul.key:not(.controls) li:not(.none) i').each(function(i, e) {
+	$('ul.key:not(.controls) li:not(.none) i').each(function(i, e) {
 		var marker = $(this).attr('class');
 		var pill = $("<div class='pill'>"+window.markerCount[marker]+"</div>");
 		$(this).next().after(pill);
@@ -570,6 +574,14 @@ $(document).on("loadCustom", function() {
 	}, 'Restore Data', 'restoreButton');
 	L.easyBar([backupButton, restoreButton]).addTo(map);
 
+	window.noteMarkers = {};
+	var noteStatus = false;
+	var noteCursorCss = null;
+	L.easyButton('fa-book', function(btn, map) {
+		if(!noteStatus) startNote();
+		else endNote();
+	}, 'Add Note', 'noteButton').addTo(map);
+
 	L.easyButton('fa-crosshairs', function(btn, map) {
 		hashParams = hash.getHashParams();
 		if(hashParams && hashParams.m) {
@@ -579,6 +591,87 @@ $(document).on("loadCustom", function() {
 			map.setView(map_center);
 		}
 	}, 'Center Highlighted Marker', 'centerButton').addTo(map);
+
+	window.getNoteKey = function (lat, lng) {
+		return lat.toFixed(3) + '_' + lng.toFixed(3);
+	};
+
+	window.getNoteIndex = function(noteKey) {
+		for(var i=0;i<notes[map_path].length;i++) {
+			if(notes[map_path][i].key == noteKey) return i;
+		}
+		return -1;
+	}
+
+	var startNote = function() {
+		console.log('starting note');
+		noteStatus = true;
+		noteCursorCss = $('.leaflet-container').css('cursor');
+		$('.leaflet-container').css('cursor', 'crosshair');
+		map.addEventListener('click', addNote);
+	};
+
+	var backupNotes = function() {
+		localStorage['notes'+map_path] = JSON.stringify(notes[map_path]);
+	};
+
+	window.saveNote = function(noteKey) {
+		var note = notes[map_path][getNoteIndex(noteKey)];
+		note.label = $('#note-label').val();
+		note.title = $('#note-title').val();
+		note.text = $('#note-text').val();
+		var marker = noteMarkers[note.key];
+		marker.bindLabel(note.label);
+		marker.bindPopup(getNotePopup(note));
+		noteMarkers[note.key] = marker;
+		backupNotes();
+	};
+
+	window.deleteNote = function(noteKey) {
+		map.removeLayer(noteMarkers[noteKey]);
+		notes[map_path].splice(getNoteIndex(noteKey), 1);
+		delete noteMarkers[noteKey];
+		backupNotes();
+		popupClose();
+	};
+
+	var getNotePopup = function(note) {
+		var popupContent =  "<div><span class=\"label\" data-i18n=\"notes.label\"></span><input type=\"text\" id=\"note-label\" data-i18n=\"[placeholder]notes.enterLabel\" value=\""+note.label+"\" /></div>";
+		popupContent += "<div><span class=\"label\" data-i18n=\"notes.title\"></span><input type=\"text\" id=\"note-title\" data-i18n=\"[placeholder]notes.enterTitle\" value=\""+note.title+"\" /></div>";
+		popupContent += "<div><span class=\"label top\" data-i18n=\"notes.note\"></span><textarea id=\"note-text\" data-i18n=\"[placeholder]notes.enterText\">"+note.text+"</textarea></div>";
+		popupContent += "<br/><button onclick=\"saveNote('"+note.key+"')\"><i class=\"fa fa-floppy-o\"></i>&nbsp;<span data-i18n=\"notes.saveNote\"></span></button>";
+		popupContent += "<button onclick=\"deleteNote('"+note.key+"')\"><i class=\"fa fa-trash-o\"></i>&nbsp;<span data-i18n=\"notes.deleteNote\"></span></button>";
+		return popupContent;
+	};
+
+	var createNote = function(note) {
+		var noteMarker = null;
+		if(note.label && note.label != '') noteMarker = L.marker(L.latLng(note.lat, note.lng), setMarker(icons['note_marker'])).bindLabel(note.label).bindPopup(getNotePopup(note)).openPopup();
+		else noteMarker = L.marker(L.latLng(note.lat, note.lng), setMarker(icons['note_marker'])).bindPopup(getNotePopup(note)).openPopup();
+		noteMarker.addTo(map);
+		noteMarkers[note.key] = noteMarker;
+	};
+
+	var addNote = function(e) {
+		var note = {key: getNoteKey(e.latlng.lat, e.latlng.lng), lat: e.latlng.lat, lng: e.latlng.lng, label:'',title:'',text:''};
+		createNote(note);
+		notes[map_path].push(note);
+		backupNotes();
+		endNote();
+		return false;
+	};
+
+	var endNote = function() {
+		noteStatus = false;
+		$('.leaflet-container').css('cursor', noteCursorCss);
+		map.removeEventListener('click');
+		console.log('stopping note');
+	};
+
+	//create saved notes on load
+	for(var i=0;i<notes[map_path].length;i++) {
+		createNote(notes[map_path][i]);
+	}
 
 	var hashParams = hash.getHashParams();
 	if(hashParams) {
--- files/js/shared.js
+++ files/js/shared.js
@@ -1,5 +1,5 @@
 $(document).on("loadShared", function () {
-    window.createMarker = function (coord, icon, label, popup, dataKey) {
+	window.createMarker = function (coord, icon, label, popup, dataKey) {
 		var mapKey = 'markers-' + map_path + '-hidden';
 		var marker = L.marker(coord, setMarker(icon)).bindLabel(label).bindPopup(popup);
 
@@ -64,6 +64,7 @@ $(document).on("loadShared", function () {
 	window.markers = {};
 	window.invisibleMarkers = {};
 	window.markerCount = {};
+	window.notes = {};
 
 	var icons = window.icons;
 	var markers = window.markers;
@@ -76,6 +77,12 @@ $(document).on("loadShared", function () {
 			localStorage[mapKey] = JSON.stringify([]);
 		}
 		invisibleMarkers[mapKey] = JSON.parse(localStorage[mapKey]);
+
+		var notesKey = 'notes'+map_path;
+		if(!localStorage[notesKey]) {
+			localStorage[notesKey] = JSON.stringify([]);
+		}
+		notes[map_path] = JSON.parse(localStorage[notesKey]);
 
 		Object.keys(data).forEach(function (dataKey) {
 			var items = data[dataKey];
@@ -91,6 +98,11 @@ $(document).on("loadShared", function () {
 			markers[dataKey] = L.layerGroup(groupItems);
 		});
 	};
+
+	icons.note_marker = L.icon({
+		iconUrl  : '../files/img/icons/note_marker.png',
+		iconSize : [20, 30]
+	});
 
 	icons.abandoned = L.icon({
 		iconUrl  : '../files/img/icons/abandoned.png',
